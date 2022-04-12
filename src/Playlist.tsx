71329606bc5bd62338d2c896e8f39ac434d061fd
mjax <matt.d.jax@gmail.com>
2022-04-12 01:43:25 -0700

    changes from pull request

--- src/Playlist.tsx
+++ src/Playlist.tsx
@@ -28,7 +28,6 @@ type FetchData = {
 
 export const Playlist: React.FC = () => {
   const playlistId = useParams().playlistId;
-  const [hoverItem, setHoverItem] = useState<null | number>(null);
   const [songList, setSongList] = useState({ index: 0, tracks: [] });
   const endpoint = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=${songList.index}`;
   var myHeaders = new Headers();
@@ -60,22 +59,15 @@ export const Playlist: React.FC = () => {
   if (data.error) {
     return <Box>404</Box>;
   }
-  const handleHover = (options: { key: number | null }) => {
-    const { key } = options;
-    if (key || key === 0) setHoverItem(key);
-    if (key === null) setHoverItem(null);
-  };
   console.log(data);
   const listSongs = (list: FetchData) => {
     return list.items.map((song, i) => (
       <ListItem
+        _hover={{ bg: 'gainsboro' }}
         display="flex"
         alignItems="center"
-        onMouseEnter={() => handleHover({ key: i })}
-        onMouseLeave={() => handleHover({ key: null })}
         borderBottom="1px"
         borderBottomColor="gainsboro"
-        bg={hoverItem === i ? 'gainsboro' : ''}
         p="1"
         key={i}
       >
@@ -92,7 +84,7 @@ export const Playlist: React.FC = () => {
           .map((artist) => ' ' + artist.name)
           .join(' / ')}`}</Text>
         <Spacer />
-        <Text pr="5" color="gray.400" fontSize="sm">
+        <Text pr="5" color="gray.400" fontSize="xs">
           {`${song.added_by.id}`}
         </Text>
       </ListItem>
--- src/Playlists.tsx
+++ src/Playlists.tsx
@@ -10,7 +10,6 @@ type FetchData = {
 
 export const Playlists: React.FC = () => {
   const navigate = useNavigate();
-  const [hoverItem, setHoverItem] = useState<null | number>(null);
   const endpoint = 'https://api.spotify.com/v1/me/playlists';
 
   const myHeaders = new Headers();
@@ -40,36 +39,30 @@ export const Playlists: React.FC = () => {
     return <Box>Playlist Loading from {endpoint}</Box>;
   }
 
-  const onClick = (options: { key: number }) => {
-    const { key } = options;
-    navigate(`/playlists/${data.items[key].id}`);
-  };
-  const handleHover = (options: { key: number | null }) => {
-    const { key } = options;
-    if (key || key === 0) setHoverItem(key);
-    if (key === null) setHoverItem(null);
+  const onClick = (options: { playlist: string }) => {
+    const { playlist } = options;
+    navigate(`/playlists/${playlist}`);
   };
 
   const listPlaylists = (list: FetchData) => {
     return list.items.map((playlist, i) => {
+      const playlistId = data.items[i].id;
       return (
         <ListItem
+          _hover={{ bg: 'gainsboro' }}
           p="2"
           display="flex"
           alignItems="baseline"
           borderBottom="1px"
           borderColor="gainsboro"
-          key={i}
-          onMouseEnter={() => handleHover({ key: i })}
-          onMouseLeave={() => handleHover({ key: null })}
-          onClick={() => onClick({ key: i })}
-          bg={hoverItem === i ? 'gainsboro' : ''}
+          key={playlistId}
+          onClick={() => onClick({ playlist: playlistId})}
         >
           <Text pl="3" fontSize="lg">
             {playlist.name}
           </Text>
           <Spacer />
-          <Text pr="3" color="gainsboro" fontSize="sm">{`${
+          <Text pr="3" color="gray.400" fontSize="sm">{`${
             playlist.tracks.total
           } ${playlist.tracks.total === 1 ? 'song' : 'songs'}`}</Text>
         </ListItem>
